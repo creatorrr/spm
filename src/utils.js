79afd290f962d7a2a8a88c9b3e4fa5e52e0d3447
Diwank Singh <diwank.singh@gmail.com>
2015-04-17 23:36:05 +0530

    Add getJSONProp util
    Signed-off-by: Diwank Singh <diwank.singh@gmail.com>

--- src/utils.js
+++ src/utils.js
@@ -7,8 +7,29 @@ const
         .done(resolve)
         .fail(reject);
     });
+  },
+
+  getJSONProp = (url, prop) => {
+    return new Promise((resolve, reject) => {
+      let found = false;
+
+      oboe(url)
+        .node(prop, value => {
+          found = true;
+          resolve(value);
+
+          // Found value; hang up
+          this.abort();
+        })
+        .done(() => {
+          if (!found)
+            reject(new Error(`Property ${ prop } not found`));
+        })
+        .fail(reject);
+    });
   };
 
 export {
-  getJSON
+  getJSON,
+  getJSONProp
 };
