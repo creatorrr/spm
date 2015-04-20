b4b8fa827ebae07a48c50729227509406cba2079
Diwank Singh <diwank.singh@gmail.com>
2015-04-20 13:07:58 +0530

    Minor changes to DependencyTree
    Signed-off-by: Diwank Singh <diwank.singh@gmail.com>

--- src/dependencyTree.js
+++ src/dependencyTree.js
@@ -1,5 +1,7 @@
+import assign from "lodash/object/assign";
 import Arboreal from "arboreal";
 import co from "co";
+import { EventEmitter } from "events";
 import filter from "lodash/collection/filter";
 import findIndex from "lodash/array/filter";
 import forEach from "lodash/collection/forEach";
@@ -10,7 +12,7 @@ import sortBy from "lodash/collection/sortBy";
 
 import Package from "./package";
 
-const mkDependencyTree = (registries=[]) =>
+const mkDependencyTree = (registries=[]) => {
   class DependencyTree extends Arboreal {
     constructor (parent, root, id) {
       // Set ID
@@ -24,12 +26,15 @@ const mkDependencyTree = (registries=[]) =>
     }
 
     buildTree () {
-      return co(this._buildTree.bind(this));
+      return co(this._buildTree.bind(this))
+        .then(this.emit.bind(this, "build"));
     }
 
     *_buildTree () {
       let deps = yield this._loadDependencies();
       forEach(deps, this.appendChild);
+
+      return this;
     }
 
     *_loadDependencies () {
@@ -78,5 +83,8 @@ const mkDependencyTree = (registries=[]) =>
         throw new Error(`No versions available for ${ condition }`);
     }
   };
+
+  return assign(DependencyTree.prototype, EventEmitter.prototype);
+};
 
 export default mkDependencyTree;
